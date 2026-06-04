0448dd715a16c5a10a62afb1a48110f30cf1463c
FrancoA21 <francomx79@gmail.com>
2026-06-03 23:35:37 -0300

    segundo commit

--- script.js
+++ script.js
@@ -61,3 +61,19 @@ lightbox.addEventListener("click", e => {
     }
 
 });
+
+// ====================
+// MENÚ HAMBURGUESA
+// ====================
+
+const hamburger =
+document.querySelector(".hamburger");
+
+const navLinks =
+document.querySelector(".nav-links");
+
+hamburger.addEventListener("click", () => {
+
+    navLinks.classList.toggle("active");
+
+});
